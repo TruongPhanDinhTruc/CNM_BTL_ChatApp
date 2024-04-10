da81673de7e6dd0844952536c5ee2a42676b45c4
TruongPhanDinhTruc <truongphandinhtruc@gmail.com>
2024-04-10 11:03:48 +0700

    hien thi send request va hien thi da ket ban

--- api/index.js
+++ api/index.js
@@ -119,10 +119,10 @@ app.post("/friend-request", async (req, res) => {
         });
 
         await User.findByIdAndUpdate(currentUserId, {
-            $pull: { sentFriendRequests: selectedUserId },
+            $push: { sentFriendRequests: selectedUserId },
         });
 
-        res.status(200);
+        res.sendStatus(200);
     } catch (error) {
         res.sendStatus(500);
     }
@@ -165,7 +165,7 @@ app.post("/friend-request/accept", async (req, res) => {
 
         await sender.save();
         await recepient.save();
-
+        // console.log(sender);
         res.status(200).json({ message: "Friend Request accepted successfully" });
     } catch (error) {
         console.log(error);
@@ -259,3 +259,37 @@ app.get("/messages/:senderId/:recepientId", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+//
+app.get("/friend-requests/sent/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findById(userId).populate("sentFriendRequests","name email image").lean();
+
+        const sentFriendRequests = user.sentFriendRequests;
+        // console.log(sentFriendRequests);
+        res.json(sentFriendRequests);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get("/friends/:userId", (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        User.findById(userId).populate("friends").then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const friendIds = user.friends.map((friend) => friend._id);
+
+            res.status(200).json(friendIds);
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
--- components/User.js
+++ components/User.js
@@ -1,20 +1,60 @@
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserType } from '../UseContext';
 
 const User = ({ item }) => {
-    const {userId, setUserId} = useContext(UserType);
+    const { userId, setUserId } = useContext(UserType);
     const [requestSent, setRequestSent] = useState(false);
-    const sendFriendRequest = async(currentUserId, selectedUserId) => {
+    const [friendRequests, setFriendRequests] = useState([]);
+    const [userFriends, setUserFriends] = useState([]);
+    useEffect(() => {
+        const fetchFriendRequests = async () => {
+            try {
+                const respone = await fetch(`http://localhost:8000/friend-requests/sent/${userId}`);
+
+                const data = await respone.json();
+                // console.log("Data: ", data);
+                if (respone.ok) {
+                    setFriendRequests(data);
+                } else {
+                    console.log("Error: ", respone.status);
+                }
+            } catch (error) {
+                console.log("Error message: ", error);
+            }
+        };
+
+        fetchFriendRequests();
+    }, []);
+
+    useEffect(() => {
+        const fetchUserFriends = async () => {
+            try {
+                const respone = await fetch(`http://localhost:8000/friends/${userId}`);
+
+                const data = await respone.json();
+                if (respone.ok) {
+                    setUserFriends(data);
+                } else {
+                    console.log("Error: ", respone.status);
+                }
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+        }
+        fetchUserFriends();
+    }, [])
+
+    const sendFriendRequest = async (currentUserId, selectedUserId) => {
         try {
             const respone = await fetch("http://localhost:8000/friend-request", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({currentUserId, selectedUserId}),
-            })
-
+                body: JSON.stringify({ currentUserId, selectedUserId }),
+            });
+            console.log("sendFriendRequest");
             if (respone.ok) {
                 setRequestSent(true);
                 console.log("request sent");
@@ -23,6 +63,10 @@ const User = ({ item }) => {
             console.log("error message", error);
         }
     };
+
+    console.log("Friends request: ", friendRequests);
+    console.log("User friends: ", userFriends);
+
     return (
         <Pressable style={styles.viewFriendItem}>
             <View>
@@ -34,10 +78,21 @@ const User = ({ item }) => {
                 <Text style={styles.txtEmail}>{item?.email}</Text>
             </View>
 
-            <Pressable onPress={() => sendFriendRequest(userId, item._id)}
-            style={styles.btnAddFriend}>
-                <Text style={styles.txtAdd}>Add Friend</Text>
-            </Pressable>
+            {userFriends.includes(item._id) ? (
+                <Pressable style={styles.btnFriend}>
+                    <Text style={styles.txtAdd}>Friends</Text>
+                </Pressable>
+            ) : requestSent || friendRequests.some((friend) => friend._id === item._id) ? (
+                <Pressable style={styles.btnSendFriend}>
+                    <Text style={styles.txtAdd}>Request send</Text>
+                </Pressable>
+            ) : (
+                <Pressable onPress={() => sendFriendRequest(userId, item._id)}
+                    style={styles.btnAddFriend}>
+                    <Text style={styles.txtAdd}>Add Friend</Text>
+                </Pressable>
+            )}
+
         </Pressable>
     )
 }
@@ -73,5 +128,17 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: "white",
         fontSize: 13
+    },
+    btnFriend: {
+        backgroundColor: "lightgreen",
+        padding: 10,
+        borderRadius: 6,
+        width: 105,
+    },
+    btnSendFriend: {
+        backgroundColor: "gray",
+        padding: 10,
+        borderRadius: 6,
+        width: 105,
     }
 })
